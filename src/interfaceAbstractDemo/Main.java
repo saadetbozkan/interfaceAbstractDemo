bdae6261939c89f46910a74de0e08f1afee42f7d
saadetbozkan <34174872+saadetbozkan@users.noreply.github.com>
2021-05-09 07:59:34 +0300

    Update Main.java

--- src/interfaceAbstractDemo/Main.java
+++ src/interfaceAbstractDemo/Main.java
@@ -11,7 +11,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		CustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
-		customerManager.save(new Customer(1, "Saadet", "Bozkan", 1996, "39934448442"));
+		customerManager.save(new Customer(1, "Saadet", "B", 1990, "111111111111"));
 		System.out.println();
 	}
 
